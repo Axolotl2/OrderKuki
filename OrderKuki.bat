d99d2cfc8b5fd7914103998b30e195f17a66d050
Axolotl2 <idanedri94@gmail.com>
2021-08-04 20:27:55 +0300

    change html file name

---
+++ OrderKuki.bat
@@ -0,0 +1,16 @@
+﻿REM Created by HTML Compiler v2.1
+REM ©2015 HTML Compiler, David Esperalta
+REM More information at www.htmlcompiler.com
+
+@ECHO OFF
+CLS
+
+REM Set our project file in PROJECT variable
+SET PROJECT="C:\Users\idan_edri\Documents\GitHub\OrderKuki\OrderKuki.hcp"
+
+REM Set HTML Compiler CMD path in COMPILER variable
+SET COMPILER="C:\Program Files (x86)\David Esperalta\HtmlCompiler\HtmlCompilerCmd.exe"
+
+REM Call the compiler passing the project file as argument
+%COMPILER% %PROJECT%
+
