9bfeae8f2fddddb405bef482cb5ebc33425164bd
Axolotl2 <idanedri94@gmail.com>
2021-08-05 22:34:50 +0300

    separate css and js from html

---
+++ js/javascript.js
@@ -0,0 +1,100 @@
+// separate js file and json file by this url: https://stackoverflow.com/questions/19706046/how-to-read-an-external-local-json-file-in-javascript
+debugger;
+
+function order(event) {
+	const sUrlNewLine = '%0A';
+	const sKukiPhone = '+972525585252', sTestPhone = '+972526241919';
+	const iName = 0, iMeal = 2, iAdditions = 3;
+	var sText = 'היי, אני רוצה להזמין בבקשה:';
+	debugger;
+	var oOrdersFromPreferences = document.getElementById('peoplePreferences');
+	
+	for (var i = 1; i < oOrdersFromPreferences.rows.length; i++) {
+		var sChecked   = oOrdersFromPreferences.rows[i].cells[0].children[0].checked;
+		var sName      = oOrdersFromPreferences.rows[i].cells[1].innerText;
+		var sType      = oOrdersFromPreferences.rows[i].cells[2].innerText;
+		var sMeal      = oOrdersFromPreferences.rows[i].cells[3].innerText;
+		var sAdditions = oOrdersFromPreferences.rows[i].cells[4].innerText;
+		var sDrinks    = oOrdersFromPreferences.rows[i].cells[5].innerText;
+		var sNotes     = oOrdersFromPreferences.rows[i].cells[6].innerText;
+
+		if (sDrinks) sDrinks = `, ${sDrinks}`;
+		if (sNotes) sNotes = `, ${sNotes}`;
+		if (sChecked && sMeal) {
+			sText += `${sUrlNewLine} ${i}. *${sType}* ${sMeal}, ${sAdditions} ${sDrinks} ${sNotes} (${sName})`;
+		}
+}
+
+var sUrl = `https://api.whatsapp.com/send?phone=${sTestPhone}&text=${sText}`;
+window.open(sUrl);
+}
+
+
+function getArrayDescriptionAsString(json, keys){
+	var resultString = "";
+
+	keys.forEach(function(object, i) {
+	    if (resultString) resultString += ', ';
+		resultString += json[i].description;
+	})
+
+	return resultString;
+}
+debugger;
+function fillPeoplePreferencesTable(){
+	var peoplePreferencesTable = document.getElementById('peoplePreferences');
+	peoplePreferences.forEach(function(object) {
+	    var tr = document.createElement('tr');
+		var typeDesc = types[object.type].description;
+		var mealDesc = meals[object.meal].description;
+		var additionsString = getArrayDescriptionAsString(additions, object.additions);
+		var drinksString = getArrayDescriptionAsString(drinks, object.drinks);
+
+	    tr.innerHTML = '<td>' + '<input type="checkbox"/>' + '</td>' +
+	        '<td>' + object.name + '</td>' +
+	        '<td>' + typeDesc + '</td>' +
+	        '<td>' + mealDesc + '</td>' +
+	        '<td>' + additionsString + '</td>' +
+	        '<td>' + drinksString + '</td>' +
+	        '<td>' + object.notes + '</td>';
+	    peoplePreferencesTable.appendChild(tr);
+		//<option value="volvo">Volvo XC90</option>
+	})
+}
+function fillTypesTable(){
+	var typesTable = document.getElementById('types');
+	types.forEach(function(object, i) {
+	    var tr = document.createElement('tr');
+	    tr.innerHTML = '<td>' + types[i].description + '</td>';
+	    typesTable.appendChild(tr);
+	})
+}
+function fillMealsTable(){
+	var mealsTable = document.getElementById('meals');
+	meals.forEach(function(object, i) {
+	    var tr = document.createElement('tr');
+	    tr.innerHTML = '<td>' + meals[i].description + '</td>';
+	    mealsTable.appendChild(tr);
+	})
+}
+function fillAdditionsTable(){
+	var additionsTable = document.getElementById('additions');
+	additions.forEach(function(object, i) {
+	    var tr = document.createElement('tr');
+	    tr.innerHTML = '<td>' + additions[i].description + '</td>';
+	    additionsTable.appendChild(tr);
+	})
+}
+function fillDrinksTable(){
+	var drinksTable = document.getElementById('drinks');
+	drinks.forEach(function(object, i) {
+	    var tr = document.createElement('tr');
+	    tr.innerHTML = '<td>' + drinks[i].description + '</td>';
+	    drinksTable.appendChild(tr);
+	})
+}
+fillPeoplePreferencesTable();
+fillTypesTable();
+fillMealsTable();
+fillAdditionsTable();
+fillDrinksTable();
