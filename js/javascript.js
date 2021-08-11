8552363ab6e113898e64f54163fc8b7e1cf32bd2
assafbres <slipknot1994@gmail.com>
2021-08-11 19:45:54 +0300

    table styling
    still needs to align the tables

--- js/javascript.js
+++ js/javascript.js
@@ -3,37 +3,45 @@ var oDB = {};
 var oOrder = {};
 
 async function loadJSONFromDB() {
-  await firebase.database().ref().get().then((data) => {
+  await firebase
+    .database()
+    .ref()
+    .get()
+    .then((data) => {
       if (data.exists()) {
         oDB = data.val();
       } else {
         console.log(`No data available`);
       }
-    }).catch((error) => {
+    })
+    .catch((error) => {
       console.error(error);
     });
 }
 function initializeDB() {
-    // Your web app's Firebase configuration
-    var firebaseConfig = {
-      apiKey: "AIzaSyDdYpByfCXzQ6Kzn922r7mkCy-aCNAs6w4",
-      authDomain: "orderkuki-c5aea.firebaseapp.com",
-      databaseURL: "https://orderkuki-c5aea-default-rtdb.europe-west1.firebasedatabase.app",
-      projectId: "orderkuki-c5aea",
-      storageBucket: "orderkuki-c5aea.appspot.com",
-      messagingSenderId: "31281277899",
-      appId: "1:31281277899:web:e7f3d1c4f15133a12a244e"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDdYpByfCXzQ6Kzn922r7mkCy-aCNAs6w4",
+    authDomain: "orderkuki-c5aea.firebaseapp.com",
+    databaseURL:
+      "https://orderkuki-c5aea-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "orderkuki-c5aea",
+    storageBucket: "orderkuki-c5aea.appspot.com",
+    messagingSenderId: "31281277899",
+    appId: "1:31281277899:web:e7f3d1c4f15133a12a244e",
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 }
 function prepareOrderMessage() {
   const sUrlNewLine = "%0A";
   // var sText = '���, ��� ���� ������ �����:';
   var sText = "מה קורה אבאלה אני רוצה להזמין:";
-  const iName = 0, iMeal = 2, iAdditions = 3;
+  const iName = 0,
+    iMeal = 2,
+    iAdditions = 3;
   var oOrderRows = document.getElementById("orderLines").rows;
-  
+
   for (var i = 1; i < oOrderRows.length; i++) {
     //var sChecked = oOrderRows[i].cells[0].children[0].checked;
     var sName = oOrderRows[i].cells[0].innerText;
@@ -53,44 +61,44 @@ function prepareOrderMessage() {
   return sText;
 }
 function saveOrderToDB() {
-    var databaseRef = firebase.database().ref();
-    var updates = {};
-    //var oOrderRows = document.getElementById("orderLines").rows;
+  var databaseRef = firebase.database().ref();
+  var updates = {};
+  //var oOrderRows = document.getElementById("orderLines").rows;
 
-    for (orderKey in oOrder) {
-        var orderLine = oOrder[orderKey];
-        orderLine["date"] = new Date();
-        //var orderData = {
-	    //	"name": orderLine.name, 
-	    //	"type": orderLine.type, 
-	    //	"meal": orderLine.meal, 
-	    //	"additions": orderLine.additions,
-	    //	"drinks": orderLine.drinks,
-	    //	"notes": orderLine.notes,
-        //    "date" : new Date()
-	    //};
+  for (orderKey in oOrder) {
+    var orderLine = oOrder[orderKey];
+    orderLine["date"] = new Date();
+    //var orderData = {
+    //	"name": orderLine.name,
+    //	"type": orderLine.type,
+    //	"meal": orderLine.meal,
+    //	"additions": orderLine.additions,
+    //	"drinks": orderLine.drinks,
+    //	"notes": orderLine.notes,
+    //    "date" : new Date()
+    //};
 
-        var newOrderKey = databaseRef.child('orders').push().key;
-        updates['orders/' + newOrderKey] = orderLine;
-    }
-    //for ( var i = 1; i < oOrderRows.length ; i++ ) {
-    //    var rowID = oOrderRows[i].children[0].innerHTML;
-    //    var row = oDB["orders"][rowID];
-    //    var orderData = {
-	//    	"name": row.name, //oOrderRows[i].children[0].innerHTML,
-	//    	"type": row.type, //oOrderRows[i].children[1].innerHTML,
-	//    	"meal": row.meal, //oOrderRows[i].children[2].innerHTML,
-	//    	"additions": row.additions,
-	//    	"drinks": row.drinks,
-	//    	"notes": row.notes, //oOrderRows[i].children[5].innerHTML,
-    //        "date" : new Date()
-	//    };
-    //
-    //    var newOrderKey = databaseRef.child('orders').push().key;
-    //    updates['orders/' + newOrderKey] = orderData;
-    //}
-    
-    return databaseRef.update(updates);
+    var newOrderKey = databaseRef.child("orders").push().key;
+    updates["orders/" + newOrderKey] = orderLine;
+  }
+  //for ( var i = 1; i < oOrderRows.length ; i++ ) {
+  //    var rowID = oOrderRows[i].children[0].innerHTML;
+  //    var row = oDB["orders"][rowID];
+  //    var orderData = {
+  //    	"name": row.name, //oOrderRows[i].children[0].innerHTML,
+  //    	"type": row.type, //oOrderRows[i].children[1].innerHTML,
+  //    	"meal": row.meal, //oOrderRows[i].children[2].innerHTML,
+  //    	"additions": row.additions,
+  //    	"drinks": row.drinks,
+  //    	"notes": row.notes, //oOrderRows[i].children[5].innerHTML,
+  //        "date" : new Date()
+  //    };
+  //
+  //    var newOrderKey = databaseRef.child('orders').push().key;
+  //    updates['orders/' + newOrderKey] = orderData;
+  //}
+
+  return databaseRef.update(updates);
 }
 function order(event) {
   const sKukiPhone = "+972525585252",
@@ -102,72 +110,78 @@ function order(event) {
   window.open(sUrl);
 }
 function addLineToOrder(orderLine) {
-    var orderTableBody = document.getElementById("orderLines").children[1];
-    var tr = document.createElement("tr");
-    var typeDesc = oDB["types"][orderLine.type]; //data.types[object.type].description;
-    var mealDesc = oDB["meals"][orderLine.meal]; //data.meals[object.meal].description;
-    var additionsString = getArrayDescriptionAsString(oDB["additions"], orderLine.additions );
-    var drinksString = getArrayDescriptionAsString(oDB["drinks"], orderLine.drinks );
-    debugger;
+  var orderTableBody = document.getElementById("orderLines").children[1];
+  var tr = document.createElement("tr");
+  var typeDesc = oDB["types"][orderLine.type]; //data.types[object.type].description;
+  var mealDesc = oDB["meals"][orderLine.meal]; //data.meals[object.meal].description;
+  var additionsString = getArrayDescriptionAsString(
+    oDB["additions"],
+    orderLine.additions
+  );
+  var drinksString = getArrayDescriptionAsString(
+    oDB["drinks"],
+    orderLine.drinks
+  );
+  debugger;
 
-    tr.innerHTML =
-      "<td>" +
-      orderLine.name +
-      "</td>" +
-      "<td>" +
-      typeDesc +
-      "</td>" +
-      "<td>" +
-      mealDesc +
-      "</td>" +
-      "<td>" +
-      additionsString +
-      "</td>" +
-      "<td>" +
-      drinksString +
-      "</td>" +
-      "<td>" +
-      orderLine.notes +
-      "</td>" +
-      "<td>" +
-      '<button class="btn btn btn-light" id="removeLineFromOrder" onclick="removeLineFromOrder(this)" type="button">הסרה</button>' +
-      "</td>";
+  tr.innerHTML =
+    "<td>" +
+    orderLine.name +
+    "</td>" +
+    "<td>" +
+    typeDesc +
+    "</td>" +
+    "<td>" +
+    mealDesc +
+    "</td>" +
+    "<td>" +
+    additionsString +
+    "</td>" +
+    "<td>" +
+    drinksString +
+    "</td>" +
+    "<td>" +
+    orderLine.notes +
+    "</td>" +
+    "<td>" +
+    '<button class="btn btn btn-outline-danger" id="removeLineFromOrder" onclick="removeLineFromOrder(this)" type="button">הסרה</button>' +
+    "</td>";
 
-    // create\replace order line
-    if (oOrder[orderLine.name]) {
-        for ( var i = 1; i < orderTableBody.children.length; i++) {
-            if (orderTableBody.children[i].children[0].innerHTML == orderLine.name) {
-                orderTableBody.replaceChild(tr, orderTableBody.children[i]);
-                break;
-            }
-        }
-    } else {
-        orderTableBody.appendChild(tr);  
-    }  
-    oOrder[orderLine.name] = orderLine;
+  // create\replace order line
+  if (oOrder[orderLine.name]) {
+    for (var i = 1; i < orderTableBody.children.length; i++) {
+      if (orderTableBody.children[i].children[0].innerHTML == orderLine.name) {
+        orderTableBody.replaceChild(tr, orderTableBody.children[i]);
+        break;
+      }
+    }
+  } else {
+    orderTableBody.appendChild(tr);
+  }
+  oOrder[orderLine.name] = orderLine;
 }
 function removeLineFromOrder(event) {
-    var orderTable = document.getElementById("orderLines");
-    var row = event.parentElement.parentElement;
-    var rowID = row.cells[0].outerText;
+  var orderTable = document.getElementById("orderLines");
+  var row = event.parentElement.parentElement;
+  var rowID = row.cells[0].outerText;
 
-    row.remove();
-    delete oOrder[rowID];
+  row.remove();
+  delete oOrder[rowID];
 }
 function addRecentOrderLineToOrder(event) {
-    var rowID = event.parentElement.parentElement.cells[0].outerText;
-    var row = oDB["orders"][rowID];
+  var rowID = event.parentElement.parentElement.cells[0].outerText;
+  var row = oDB["orders"][rowID];
 
-    var orderLine = {
-		"name": row.name,
-		"type": row.type,
-		"meal": row.meal,
-		"additions": (row.additions != null) ? row.additions : [],
-		"drinks": (row.drinks != null) ? row.drinks : [],
-		"notes": row.notes
-	};
+  var orderLine = {
+    name: row.name,
+    type: row.type,
+    meal: row.meal,
+    additions: row.additions != null ? row.additions : [],
+    drinks: row.drinks != null ? row.drinks : [],
+    notes: row.notes,
+  };
 
-    addLineToOrder(orderLine);
+  addLineToOrder(orderLine);
 }
 function getArrayDescriptionAsString(descriptionList, keys) {
   var resultString = "";
@@ -176,7 +190,7 @@ function getArrayDescriptionAsString(descriptionList, keys) {
 
   keys.forEach(function (object) {
     if (resultString) resultString += ", ";
-    resultString += descriptionList[object];    
+    resultString += descriptionList[object];
   });
 
   //firebase.database().ref().child(tableName).get().then((data) => {
@@ -197,7 +211,7 @@ function getArrayDescriptionAsString(descriptionList, keys) {
   //  if (resultString) resultString += ", ";
   //  resultString += json[i].description;
   //});
-  
+
   return resultString;
 }
 //function fillPeoplePreferencesTable() {
@@ -239,15 +253,15 @@ function getArrayDescriptionAsString(descriptionList, keys) {
 //  });
 //}
 function fillTableFromDB(tableName) {
-    var rows = oDB[tableName];
-    var table = document.getElementById(tableName);
-    rows.forEach(function (object, i) {
-        var tr = document.createElement("tr");
-        tr.innerHTML = "<td>" + rows[i] + "</td>";
-        // tr.innerHTML = `<td> <img class="imageContainer" src = ${data.types[i].img}> ${data.types[i].description} </td>`;
-        // tr.innerHTML = `<td> <img class="img-fluid of img-thumbnail" src = ${data.types[i].img} <div> ${data.types[i].description} </div> </td>`;
-        table.appendChild(tr);
-    });
+  var rows = oDB[tableName];
+  var table = document.getElementById(tableName);
+  rows.forEach(function (object, i) {
+    var tr = document.createElement("tr");
+    tr.innerHTML = "<td>" + rows[i] + "</td>";
+    // tr.innerHTML = `<td> <img class="imageContainer" src = ${data.types[i].img}> ${data.types[i].description} </td>`;
+    // tr.innerHTML = `<td> <img class="img-fluid of img-thumbnail" src = ${data.types[i].img} <div> ${data.types[i].description} </div> </td>`;
+    table.appendChild(tr);
+  });
 
   //firebase.database().ref().child(tableName).get().then((data) => {
   //    if (data.exists()) {
@@ -267,73 +281,81 @@ function fillTableFromDB(tableName) {
   //  });
 }
 function getRecentOrderPerName(ordersFromDB) {
-    var orders = [], peopleRecentOrders = [];
+  var orders = [],
+    peopleRecentOrders = [];
 
-    for (orderKey in ordersFromDB) {
-        var order = ordersFromDB[orderKey];
-        order["id"] = orderKey;
-        orders.push( order );
+  for (orderKey in ordersFromDB) {
+    var order = ordersFromDB[orderKey];
+    order["id"] = orderKey;
+    orders.push(order);
+  }
+  orders.sort((a, b) => {
+    if (a["name"] === b["name"] && a["date"] === b["date"]) {
+      return 0;
+    } else if (a["name"] === b["name"]) {
+      return a["date"] > b["date"] ? -1 : 1;
+    } else {
+      return a["name"] < b["name"] ? -1 : 1;
     }
-    orders.sort((a,b) => {
-        if (a["name"] === b["name"] && a["date"] === b["date"]) {
-            return 0;
-        } else if (a["name"] === b["name"]) {
-            return (a["date"] > b["date"]) ? -1 : 1;
-        } else {
-            return (a["name"] < b["name"]) ? -1 : 1;
-        }
-    });
-    orders = orders.reduce((order, val) => {
-        if (Object.keys(order).includes(val.name)) return order;
-    
-        order[val.name] = orders.filter(g => g.name === val.name); 
-        return order;
-    }, {});
-    for (name in orders) {
-        peopleRecentOrders.push( orders[name][0] );
-    }
-    
-    return peopleRecentOrders;
+  });
+  orders = orders.reduce((order, val) => {
+    if (Object.keys(order).includes(val.name)) return order;
+
+    order[val.name] = orders.filter((g) => g.name === val.name);
+    return order;
+  }, {});
+  for (name in orders) {
+    peopleRecentOrders.push(orders[name][0]);
+  }
+
+  return peopleRecentOrders;
 }
 function preparePeopleRecentOrdersTable(orders) {
-    var recentOrdersTableBody = document.getElementById("peopleRecentOrders").children[1];
+  var recentOrdersTableBody =
+    document.getElementById("peopleRecentOrders").children[1];
 
-    orders.forEach(function (object) {
-        var tr = document.createElement("tr");
-        var typeDesc = oDB["types"][object.type]; //data.types[object.type].description;
-        var mealDesc = oDB["meals"][object.meal]; //data.meals[object.meal].description;
-        var additionsString = getArrayDescriptionAsString(oDB["additions"], object.additions );
-        var drinksString = getArrayDescriptionAsString(oDB["drinks"], object.drinks );
+  orders.forEach(function (object) {
+    var tr = document.createElement("tr");
+    var typeDesc = oDB["types"][object.type]; //data.types[object.type].description;
+    var mealDesc = oDB["meals"][object.meal]; //data.meals[object.meal].description;
+    var additionsString = getArrayDescriptionAsString(
+      oDB["additions"],
+      object.additions
+    );
+    var drinksString = getArrayDescriptionAsString(
+      oDB["drinks"],
+      object.drinks
+    );
 
-        tr.innerHTML =
-          '<td style="display:none;">' +
-          object.id +
-          "</td>" +
-          "<td>" +
-          object.name +
-          "</td>" +
-          "<td>" +
-          typeDesc +
-          "</td>" +
-          "<td>" +
-          mealDesc +
-          "</td>" +
-          "<td>" +
-          additionsString +
-          "</td>" +
-          "<td>" +
-          drinksString +
-          "</td>" +
-          "<td>" +
-          object.notes +
-          "</td>" + 
-          "<td>" +
-          //'<input type="checkbox"/>' +
-          '<button class="btn btn btn-light" id="addRecentOrderLineToOrder" onclick="addRecentOrderLineToOrder(this)" type="button">הוספה</button>' +
-          "</td>";
+    tr.innerHTML =
+      '<td style="display:none;">' +
+      object.id +
+      "</td>" +
+      "<td class='bg-light'>" +
+      object.name +
+      "</td>" +
+      "<td class='bg-light'>" +
+      typeDesc +
+      "</td>" +
+      "<td class='bg-light'>" +
+      mealDesc +
+      "</td>" +
+      "<td class='bg-light'>" +
+      additionsString +
+      "</td>" +
+      "<td class='bg-light'>" +
+      drinksString +
+      "</td>" +
+      "<td class='bg-light'>" +
+      object.notes +
+      "</td>" +
+      "<td class='bg-light'>" +
+      //'<input type="checkbox"/>' +
+      '<button class="btn btn btn-outline-primary" id="addRecentOrderLineToOrder" onclick="addRecentOrderLineToOrder(this)" type="button">הוספה</button>' +
+      "</td>";
 
-        recentOrdersTableBody.appendChild(tr);
-    });
+    recentOrdersTableBody.appendChild(tr);
+  });
 }
 function fillPeopleRecentOrdersTable() {
   preparePeopleRecentOrdersTable(getRecentOrderPerName(oDB["orders"]));
@@ -398,54 +420,53 @@ function fillPeopleRecentOrdersTable() {
 //  });
 //}
 function createCollapsibleMenu() {
-	
-    var menuDiv = document.getElementById("menu");
-	
-	//for ( type in oDB["types"] ) {
-	//	var headerDiv = document.createElement("div");
-	//	headerDiv.class = "collapsible";
-	//	var contentDiv = document.createElement("div");
-	//	contentDiv.class = "content";
-	//	var testH1 = document.createElement("h1");
-	//	testH1.textContent = oDB["types"][type];
-	//	var test2H1 = document.createElement("h2");
-	//	test2H1.textContent = "collapse me";
-	//	var button = document.createElement("button");
-	//	button.class = "btn btn btn-light";
-	//	button.onclick = "addMenuItemToOrder(button)";
-	//	button.type = "button";
-	//	button.textContent = "הוספה";
-	//	
-	//	headerDiv.appendChild(testH1);
-	//	headerDiv.appendChild(button);
-	//	contentDiv.appendChild(test2H1);
-    //
-	//	headerDiv.addEventListener("click", function() {
-	//		this.classList.toggle("active");
-	//		var content = this.nextElementSibling;
-	//		if (content.style.display === "block") {
-	//		content.style.display = "none";
-	//		} else {
-	//		content.style.display = "block";
-	//		}
-	//	});
-	//	menuDiv.appendChild(headerDiv);    
-	//	menuDiv.appendChild(contentDiv);    
-	//}
+  var menuDiv = document.getElementById("menu");
+
+  //for ( type in oDB["types"] ) {
+  //	var headerDiv = document.createElement("div");
+  //	headerDiv.class = "collapsible";
+  //	var contentDiv = document.createElement("div");
+  //	contentDiv.class = "content";
+  //	var testH1 = document.createElement("h1");
+  //	testH1.textContent = oDB["types"][type];
+  //	var test2H1 = document.createElement("h2");
+  //	test2H1.textContent = "collapse me";
+  //	var button = document.createElement("button");
+  //	button.class = "btn btn btn-light";
+  //	button.onclick = "addMenuItemToOrder(button)";
+  //	button.type = "button";
+  //	button.textContent = "הוספה";
+  //
+  //	headerDiv.appendChild(testH1);
+  //	headerDiv.appendChild(button);
+  //	contentDiv.appendChild(test2H1);
+  //
+  //	headerDiv.addEventListener("click", function() {
+  //		this.classList.toggle("active");
+  //		var content = this.nextElementSibling;
+  //		if (content.style.display === "block") {
+  //		content.style.display = "none";
+  //		} else {
+  //		content.style.display = "block";
+  //		}
+  //	});
+  //	menuDiv.appendChild(headerDiv);
+  //	menuDiv.appendChild(contentDiv);
+  //}
 }
 async function initialize() {
-    initializeDB();
-    await loadJSONFromDB();
-    fillPeopleRecentOrdersTable();
-    createCollapsibleMenu();
-    fillTableFromDB("types");
-    fillTableFromDB("meals");
-    fillTableFromDB("additions");
-    fillTableFromDB("drinks");
-    //fillTypesTable();
-    //fillMealsTable();
-    //fillAdditionsTable();
-    //fillDrinksTable();
+  initializeDB();
+  await loadJSONFromDB();
+  fillPeopleRecentOrdersTable();
+  createCollapsibleMenu();
+  fillTableFromDB("types");
+  fillTableFromDB("meals");
+  fillTableFromDB("additions");
+  fillTableFromDB("drinks");
+  //fillTypesTable();
+  //fillMealsTable();
+  //fillAdditionsTable();
+  //fillDrinksTable();
 }
 
 initialize();
